705be5941c2f10bcb95cd64bf8a5d4202154f495
tollispa <112690202+tollispa@users.noreply.github.com>
2023-06-20 19:13:37 +0200

    added message route

---
+++ my-app/server/controllers/message.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const sendMessage = express.Router();
+const user = require("../schemas/user")
+const Message = require("../schemas/message")
+
+
+const formattedDate = require("../dateVariable")
+
+sendMessage.post("/", async (req, res) => {
+    const messageSent = req.session.userId
+    const {recieverName, title, message} = req.body
+
+  try {
+        
+        const findUser = await user.find({ _id: `${messageSent}` }).lean().select('username').exec()
+   
+        console.log(findUser[0].username, recieverName, title, message, formattedDate)
+    const createMessage = new Message({
+            from: findUser[0].username,
+            to: recieverName,
+            title: title,
+            message: message,
+            sendAtDate: formattedDate
+          
+        });
+    await createMessage.save();
+        res.send({message: "Meddelandet skickades!"})
+        
+    } catch (err) {
+        
+        res.send(err)
+    }
+   
+} )
+
+module.exports = sendMessage
