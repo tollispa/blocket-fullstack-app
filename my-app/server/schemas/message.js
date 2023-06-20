7d69b66ec96483f42b5b8a539a79697905f83866
tollispa <112690202+tollispa@users.noreply.github.com>
2023-06-20 19:13:52 +0200

    message schema

---
+++ my-app/server/schemas/message.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const messageSchema = new mongoose.Schema({
+ 
+    from: {
+        type: String,
+        required: true
+    }, title: {
+        type: String,
+        required: true
+    },
+    
+    to: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    sendAtDate: {
+        type: String,
+        required: true
+    }
+},{ strict: "throw" })
+const Message = mongoose.model('Messages', messageSchema);
+
+module.exports = Message
