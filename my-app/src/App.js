eef880517e3e43bce24ee106be30d4dce92efa95
tollispa <112690202+tollispa@users.noreply.github.com>
2023-06-20 19:14:13 +0200

    added user prop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,40 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import axios from "axios"
 import Posts from "./components/Posts";
 import CreatePost from "./components/CreatePost";
+import SinglePost from "./components/SinglePost";
+import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   axios.defaults.withCredentials = true;
+  const [loggedInUser, setLoggedInUser] = useState("")
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/isLoggedIn")
+    .then((res) => {
+        if(res.status === 200) {
+         
+            setLoggedInUser(res.data) 
+            
+            
+        }
+        console.log(res)
+    }).catch((err) => console.log(err))
+}, [])
+
+console.log(loggedInUser, "Inloggad")
   return (
     <>
     <BrowserRouter>
-    <Navbar/>
+    <Navbar user={loggedInUser}/>
     <Routes>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
-      <Route path="/posts" element={<Posts/>}/>
+      <Route path="/posts" element={<Posts user={loggedInUser}/>}/>
       <Route path="/createpost" element={<CreatePost/>}/>
+      <Route path="/post/:id" element={<SinglePost/>}/>
+
 
 
     </Routes>
