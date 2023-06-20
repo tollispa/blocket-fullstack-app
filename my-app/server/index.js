af794b65045765d16061938a13144211e34009b6
tollispa <112690202+tollispa@users.noreply.github.com>
2023-06-20 19:13:19 +0200

    imported cmps

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -6,6 +6,9 @@ const register = require("./Auth/register")
 const login = require("./Auth/login")
 const createPost = require("./controllers/createPost")
 const getPosts = require("./controllers/getPosts")
+const getSinglePost = require("./controllers/singlePost")
+const sendMessage = require("./controllers/message")
+
 
 
 const app = express()
@@ -40,6 +43,16 @@ app.use("/register", register)
 app.use("/login", login)
 app.use("/createpost", createPost)
 app.use("/getposts", getPosts)
+app.use("/singlepost", getSinglePost)
+app.use("/sendmessage", sendMessage)
+
+app.get("/isLoggedIn", (req, res) => {
+    if (!req.session.userId) {
+        res.sendStatus(404)
+    }else {
+        res.send(req.session.username)
+    }
+})
 
 app.listen(4000, () => {
     console.log("Server started on port 4000")
